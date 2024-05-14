1356491f99bc52a50276fa43e2fcae99c2a1e0bf
Pjoter <voxli682@gmail.com>
2024-05-14 20:34:57 +0200

    Dodano możliwość rejestracji

--- register.php
+++ register.php
@@ -1,43 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Formularz Rejestracji</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container">
-        <div class="row text-center mt-5">
-            <h1>Rejestracja</h1>
-        </div>
-        <div class="row">
-            <form action="login.php">
-                <div class="mb-3 col-4 offset-4">
-                    <label class="form-label" for="emailInput">Email:</label>
-                    <input class="form-controll w-100" type="email" name="email" id="emailInput">
-                </div>
-                <div class="mb-3 col-4 offset-4">
-                    <label class="form-label" for="passwordInput">Hasło:</label>
-                    <input class="form-controll w-100" type="password" name="password" id="passwordInput">
-                </div>
-                <div class="mb-3 col-4 offset-4">
-                    <label class="form-label" for="repeatPasswordInput">Powtórz hasło:</label>
-                    <input class="form-controll w-100" type="repeatPassword" name="repeatPassword" id="repeatPasswordInput">
-                </div>
-                <div class="mb-3 col-4 offset-4">
-                    <label class="form-label" for="checkbox">Potwierdź przetworzenie danych ;D</label>
-                    <input type="checkbox" id="checkbox">
-                </div>
-                
-                <div class="mb-3 col-4 offset-4">
-                    <button type="submit" class="btn btn-primary w-100">Zaloguj</button>
-                </div>
+<div id="container">
+    <h1 class="text-center mt-5 mb-5">Rejestracja</h1>
+        <div id="loginFrom" class="col-4 offset-4 mt-5">
+            <form action="rejestracja.php" method="post">
+                <label for="emailInput" class="from-label">Adres e-mail:</label>
+                <input type="email" class="from-controll mb-3" name="email" id="emailInput">
+                <label for="passwordInput" class="from-label">Haslo:</label>
+                <input type="password" class="from-controll mb-3" name="password" id="passwordInput">
+                <button type="submit" class="btn btn-primary w-100 mt-3">Zajerestruj</button>
             </form>
+            <?php
+            if (isset($result)) {
+                if ($result) {
+                    echo "Udało się utworzyć nowe konto";
+                } else {
+                    echo "Nie udało się utworzyć nowego konta";
+                }
+            }
+
+            ?>
+
         </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+</div>
 </body>
 </html>
